066da75c64f61ff5bb83309bef8f225437236311
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-06-17 22:13:17 +0530

    Create 3189-FindChampionIi

---
+++ 3189-FindChampionIi/3189-FindChampionIi.java
@@ -0,0 +1,22 @@
+// Last updated: 17/06/2025, 22:13:17
+class Solution {
+    public int findChampion(int n, int[][] edges) {
+        int[] indegree = new int[n];
+
+        for (int[] edge : edges) {
+            indegree[edge[1]]++;
+        }
+
+        int cnt = 0;
+        int ind = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                cnt++;
+                ind = i;
+            }
+        }
+
+        return (cnt == 1) ? ind : -1;
+    }
+}
