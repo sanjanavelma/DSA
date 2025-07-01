408c06fe98613aa4a56ca6fd7ca0148a3572b9d0
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-07-01 13:30:09 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/version/python/189-RotateArray_20250701_133009.py
@@ -0,0 +1,6 @@
+# Last updated: 01/07/2025, 13:30:09
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        for i in range(k):
+            nums.insert(0,nums[-1])
+            nums.pop()
