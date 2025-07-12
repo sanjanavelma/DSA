7abe3a9c76574a62e38dc863202aa3ab62409304
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-07-12 22:51:00 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/version/python/14-LongestCommonPrefix_20250712_225100.py
@@ -0,0 +1,8 @@
+# Last updated: 12/07/2025, 22:51:00
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return s[:i]
+        return strs[0]
