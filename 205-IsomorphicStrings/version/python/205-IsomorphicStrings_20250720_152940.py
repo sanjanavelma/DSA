f7a6d68643259fa9a458c0ab08ccb65275683361
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-07-20 15:29:39 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/version/python/205-IsomorphicStrings_20250720_152940.py
@@ -0,0 +1,13 @@
+# Last updated: 20/07/2025, 15:29:40
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        char_map = {}
+        for sc, tc in zip(s, t):
+            if sc in char_map:
+                if char_map[sc] != tc:
+                    return False
+            elif tc in char_map.values():
+                return False
+            char_map[sc] = tc
+        return True
+        
