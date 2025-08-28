182479aa4a5f9a9ed9bc879ce5acd0755b4126b2
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-08-28 13:40:33 +0530

    Create 706-DesignHashMap

---
+++ 706-DesignHashMap/version/python/706-DesignHashMap_20250828_134033.py
@@ -0,0 +1,14 @@
+# Last updated: 28/08/2025, 13:40:33
+class MyHashMap:
+
+    def __init__(self):
+        self.map = [-1] * 1000001
+
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+
+    def get(self, key: int) -> int:
+        return self.map[key]
+
+    def remove(self, key: int) -> None:
+        self.map[key] = -1
