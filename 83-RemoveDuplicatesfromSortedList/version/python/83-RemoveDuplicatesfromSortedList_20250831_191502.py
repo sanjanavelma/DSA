9d0f14d73e8aec0fe43ba60a9726c2567381b2c4
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-08-31 19:15:02 +0530

    Create 83-RemoveDuplicatesfromSortedList

---
+++ 83-RemoveDuplicatesfromSortedList/version/python/83-RemoveDuplicatesfromSortedList_20250831_191502.py
@@ -0,0 +1,12 @@
+# Last updated: 31/08/2025, 19:15:02
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        res = head
+
+        while head and head.next:
+            if head.val == head.next.val:
+                head.next = head.next.next
+            else:
+                head = head.next
+        
+        return res
