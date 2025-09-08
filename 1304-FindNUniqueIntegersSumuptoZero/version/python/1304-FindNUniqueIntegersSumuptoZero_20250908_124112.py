d2d573245ffa8d0fa2454d32733485ece5198325
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-09-08 12:41:12 +0530

    Create 1304-FindNUniqueIntegersSumuptoZero

---
+++ 1304-FindNUniqueIntegersSumuptoZero/version/python/1304-FindNUniqueIntegersSumuptoZero_20250908_124112.py
@@ -0,0 +1,16 @@
+# Last updated: 08/09/2025, 12:41:12
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        # a+b=n
+        # a=n-b
+        def check(val):
+            while val:
+                tmp=val%10
+                if tmp==0:
+                    return False
+                val//=10
+            return True
+        for b in range(1,n): 
+            a=n-b
+            if check(a) and check(b):
+                return [a,b]
