8028d6a459c1822fc20909f65bc7bde8737a5a29
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-09-10 14:52:45 +0530

    Create 1431-KidsWiththeGreatestNumberofCandies

---
+++ 1431-KidsWiththeGreatestNumberofCandies/version/python/1431-KidsWiththeGreatestNumberofCandies_20250910_145246.py
@@ -0,0 +1,12 @@
+# Last updated: 10/09/2025, 14:52:46
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extra: int) -> List[bool]:
+        currmax = max(candies)
+        ans = []
+        for i in candies:
+            if currmax <= i+extra:
+                ans.append(True)
+            else:
+                ans.append(False)
+        
+        return ans
