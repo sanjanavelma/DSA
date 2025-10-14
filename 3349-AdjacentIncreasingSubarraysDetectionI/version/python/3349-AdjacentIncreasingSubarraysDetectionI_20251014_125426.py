5411c9d9cea3bd607ce4859fa43e6ca4f4fad4f9
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-10-14 12:54:26 +0530

    Create 3349-AdjacentIncreasingSubarraysDetectionI

---
+++ 3349-AdjacentIncreasingSubarraysDetectionI/version/python/3349-AdjacentIncreasingSubarraysDetectionI_20251014_125426.py
@@ -0,0 +1,14 @@
+# Last updated: 14/10/2025, 12:54:26
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        knew = k - 1
+        if knew == 0:
+            return True
+        for j in range(k + 1, len(nums)):
+            if nums[j] > nums[j - 1] and nums[j - k] > nums[j - k - 1]:
+                knew -= 1
+            else:
+                knew = k - 1
+            if knew == 0:
+                return True
+        return False
