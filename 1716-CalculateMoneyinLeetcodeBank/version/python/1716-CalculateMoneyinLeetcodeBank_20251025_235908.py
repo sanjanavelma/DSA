aef925326acede7929f86def13a38b0c2d524c46
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-10-25 23:59:06 +0530

    Create 1716-CalculateMoneyinLeetcodeBank

---
+++ 1716-CalculateMoneyinLeetcodeBank/version/python/1716-CalculateMoneyinLeetcodeBank_20251025_235908.py
@@ -0,0 +1,6 @@
+# Last updated: 25/10/2025, 23:59:08
+class Solution(object):
+    def totalMoney(self, n):
+        weeks, days = divmod(n, 7)
+        amount = (weeks*(weeks-1)//2)*7 + weeks*28 + (days*(days+1)//2) + weeks*days
+        return amount
