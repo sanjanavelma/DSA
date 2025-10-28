ee44bddd8be34dc0e52855aa07fcb2843b252636
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-10-29 00:59:14 +0530

    Create 3354-MakeArrayElementsEqualtoZero

---
+++ 3354-MakeArrayElementsEqualtoZero/version/python/3354-MakeArrayElementsEqualtoZero_20251029_005914.py
@@ -0,0 +1,13 @@
+# Last updated: 29/10/2025, 00:59:14
+class Solution(object):
+    def countValidSelections(self, nums):
+        total, ls, cases = sum(nums), 0, 0
+        for i in range(len(nums)):
+            rs = total - ls - nums[i]
+            if nums[i] == 0:
+                if ls == rs:
+                    cases += 2
+                elif ls == rs - 1 or ls - 1 == rs:
+                    cases += 1
+            ls += nums[i]
+        return cases
