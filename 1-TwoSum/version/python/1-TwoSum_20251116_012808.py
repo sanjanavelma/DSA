dcfa1f51d7f3e27f949cc576dbdc13efa7be2888
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-11-16 01:28:08 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/python/1-TwoSum_20251116_012808.py
@@ -0,0 +1,7 @@
+# Last updated: 16/11/2025, 01:28:08
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j] 
