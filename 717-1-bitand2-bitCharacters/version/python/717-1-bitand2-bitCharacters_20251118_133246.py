7d263c7975b35d3466650c2435d85d34adcd1b3a
Sanjana_velma <119407040+sanjanavelma@users.noreply.github.com>
2025-11-18 13:32:47 +0530

    Create 717-1-bitand2-bitCharacters

---
+++ 717-1-bitand2-bitCharacters/version/python/717-1-bitand2-bitCharacters_20251118_133246.py
@@ -0,0 +1,8 @@
+# Last updated: 18/11/2025, 13:32:46
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n = len(bits)
+        i = 0
+        while i < n - 1:
+            i += bits[i] + 1
+        return i == n - 1
